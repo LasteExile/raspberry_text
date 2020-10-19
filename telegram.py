5909fbcb1c200bb1f9cee921e94cdf8293d01b5e
lasteexile <bbriz020903@gmail.com>
2020-10-19 14:56:32 +0300

    Changed file's name

--- telegram.py
+++ telegram.py
@@ -1,15 +1,14 @@
 import os
-import matrix_text
+import text
 
 from pyrogram import Client, filters
 
-from matrix_chars import chars
+from chars import chars
 
 
 api_id = os.environ.get('api_id') 
 api_hash = os.environ.get('api_hash') 
-a
-
+print(os.environ)
 app = Client("matrix_bot", api_id, api_hash)
 
 color = (10, 10, 10)
@@ -17,6 +16,6 @@ color = (10, 10, 10)
 @app.on_message(filters.private & filters.incoming)
 def new_message(client, message):
     if all(char in chars.keys() for char in message.text):
-        matrix_text.main(message.text, color)
+        text.main(message.text, color)
 
 app.run()
